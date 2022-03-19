14b67975c7c0cc9965cf669e93e3abb51baa5aea
Mariana Cruz <65827628+marianafcruz17@users.noreply.github.com>
2022-03-19 11:00:30 -0300

    Add files via upload

---
+++ altera_rgb.m
@@ -0,0 +1,12 @@
+function altera_rgb(x)
+%procedimento para alterar o vermelho (incrementa)
+[L,C] = size(x)
+y = x;
+
+for nivel_vermelho = 0:40:255
+    y(:,:,1) = nivel_vermelho;
+    close all
+    imshow(y)
+    pause;
+    close all
+end
